aefbf5bfad60dd2afae0eb9ecf5c06966cee5b79
Nate Brennand <nate.brennand@clever.com>
2015-07-21 07:05:51 +0000

    add testing credentials for use with local dynamo

---
+++ test_creds.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+export DOMAIN=clever.com
+export PORT=:5000
+export AWS_ACCESS_KEY=abc
+export AWS_SECRET_KEY=abc
+export DYNAMO_TABLE=users
+export DYNAMO_ENDPOINT=http://localhost:8000
+export DYNAMO_REGION=us-west-1
+
