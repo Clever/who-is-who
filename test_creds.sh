d0ca337bf0b7c29eb89213065ba73f961938a0ae
Xavi Ramirez <xavi.ramirez@clever.com>
2016-03-17 00:31:17 +0000

    Synced envvars in launch yaml with those in pillar.

--- test_creds.sh
+++ test_creds.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
-export DOMAIN=clever.com
 export PORT=:5000
-export AWS_ACCESS_KEY=abc
-export AWS_SECRET_KEY=abc
+export AWS_ACCESS_KEY_ID=abc
+export AWS_SECRET_ACCESS_KEY=abc
 export DYNAMO_TABLE=who-is-who
 export DYNAMO_ENDPOINT=http://localhost:8000
 export DYNAMO_REGION=us-west-1
