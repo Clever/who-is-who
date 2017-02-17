c299c3ce6aec9bffd9a976c4f623c791291a48bb
Nathan Leiby <nathan.leiby@clever.com>
2017-02-17 13:47:03 -0800

    integration_tests: update dynamodb_local url

--- tests/run_integration_tests.sh
+++ tests/run_integration_tests.sh
@@ -8,12 +8,12 @@ then
 	if [ `uname` = "Darwin" ] ; then
 		# this will prompt for java to be installed if necessary
 		java -version
-	else  
+	else
     	sudo apt-get update && sudo apt-get install -y default-jre
     fi
     mkdir -p dynamo-local
     echo "Downloading dynamo server..."
-    curl -L -k --url http://dynamodb-local.s3-website-us-west-2.amazonaws.com/dynamodb_local_latest.tar.gz -o dynamo-local/dynamodb_local_latest.tar.gz
+    curl -L -k --url https://s3-us-west-2.amazonaws.com/dynamodb-local/dynamodb_local_latest.tar.gz -o dynamo-local/dynamodb_local_latest.tar.gz
     tar -zxvf dynamo-local/dynamodb_local_latest.tar.gz -C dynamo-local/
 fi
 
