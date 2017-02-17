13aa9fd3dc3a2992d7df4299cbb18e65c9a9ca49
Nathan Leiby <nathan.leiby@clever.com>
2017-02-17 12:24:26 -0800

    go-client: backup before migrating to node

---
+++ go-client/client.go
@@ -0,0 +1,102 @@
+package whoswho
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+// Client represents the API client. It holds the endpoint the server is expected to be
+// located at.
+type Client struct {
+	endpoint string
+}
+
+// NewClient crafts a new API client.
+func NewClient(endpoint string) Client {
+	return Client{endpoint}
+}
+
+// User represents the data collected and served by who's who
+type User struct {
+	FirstName string `json:"first_name"`       // Slack
+	LastName  string `json:"last_name"`        // Slack
+	Email     string `json:"email"`            // Slack
+	Slack     string `json:"slack"`            // Slack
+	Phone     string `json:"phone"`            // Slack
+	AWS       string `json:"aws"`              // first initial + last name
+	Github    string `json:"github,omitempty"` // Github
+}
+
+// GetUserList makes a call to /list and returns all users.
+func (c Client) GetUserList() ([]User, error) {
+	resp, err := http.Get(c.endpoint + "/list")
+	if err != nil {
+		return []User{}, fmt.Errorf("list users call failed => {%s}", err)
+	}
+
+	defer resp.Body.Close()
+	var users []User
+	err = json.NewDecoder(resp.Body).Decode(&users)
+	if err != nil {
+		return []User{}, fmt.Errorf("json unmarshaling failed => {%s}", err)
+	}
+
+	return users, nil
+}
+
+// UserByAWS finds a user based on their AWS username.
+func (c Client) UserByAWS(username string) (User, error) {
+	resp, err := http.Get(c.endpoint + fmt.Sprintf("/alias/aws/%s", username))
+	if err != nil {
+		return User{}, fmt.Errorf("aws alias match call failed => {%s}", err)
+	}
+
+	return returnUser(resp)
+}
+
+// UserByGithub finds a user based on their Github username.
+func (c Client) UserByGithub(username string) (User, error) {
+	resp, err := http.Get(c.endpoint + fmt.Sprintf("/alias/github/%s", username))
+	if err != nil {
+		return User{}, fmt.Errorf("aws alias match call failed => {%s}", err)
+	}
+
+	return returnUser(resp)
+}
+
+// UserBySlack finds a user based on their Slack username.
+func (c Client) UserBySlack(username string) (User, error) {
+	resp, err := http.Get(c.endpoint + fmt.Sprintf("/alias/slack/%s", username))
+	if err != nil {
+		return User{}, fmt.Errorf("slack alias match call failed => {%s}", err)
+	}
+
+	return returnUser(resp)
+}
+
+// UserByEmail finds a user based on their email.
+func (c Client) UserByEmail(email string) (User, error) {
+	resp, err := http.Get(c.endpoint + fmt.Sprintf("/alias/email/%s", email))
+	if err != nil {
+		return User{}, fmt.Errorf("email match call failed => {%s}", err)
+	}
+
+	return returnUser(resp)
+}
+
+// returnUser performs necessary unmarshaling and response parsing for all alias endpoints.
+func returnUser(resp *http.Response) (User, error) {
+	if resp.StatusCode != http.StatusOK {
+		return User{}, fmt.Errorf("%d status code", resp.StatusCode)
+	}
+
+	var u User
+	defer resp.Body.Close()
+	err := json.NewDecoder(resp.Body).Decode(&u)
+	if err != nil {
+		return User{}, fmt.Errorf("json unmarshaling failed => {%s}", err)
+	}
+
+	return u, nil
+}
