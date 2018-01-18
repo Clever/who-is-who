a1f8564796bcd47cc897cb032c94d079e203323e
Nathan Leiby <nathan.leiby@clever.com>
2018-01-17 17:01:59 -0800

    Add Team to go-client

--- go-client/client.go
+++ go-client/client.go
@@ -22,15 +22,16 @@ func NewClient(endpoint string) Client {
 
 // User represents the data collected and served by who's who
 type User struct {
-	FirstName string `json:"first_name"`       // Slack
-	LastName  string `json:"last_name"`        // Slack
-	Email     string `json:"email"`            // Slack
+	FirstName string `json:"first_name"`       // FirstName
+	LastName  string `json:"last_name"`        // LastName
+	Email     string `json:"email"`            // Email
 	Slack     string `json:"slack"`            // Slack
-	Phone     string `json:"phone"`            // Slack
-	SlackID   string `json:"slack_id"`         // Slack
+	Phone     string `json:"phone"`            // Phone
+	SlackID   string `json:"slack_id"`         // Slack ID (not Slack alias)
 	AWS       string `json:"aws,omitempty"`    // first initial + last name
-	Github    string `json:"github,omitempty"` // Github
+	Github    string `json:"github,omitempty"` // Github username
 	Active    bool   `json:"active"`           // Is user currently at Clever
+	Team      string `json:"team,omitempty"`   // What team is the user on
 }
 
 // GetUserList makes a call to /list and returns all users.
