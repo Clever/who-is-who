c1e8b45868c4258fa31ab393cffd56721d4f7e47
Renato <renato.prime@clever.com>
2020-10-07 21:48:25 -0700

    prettify

--- server.js
+++ server.js
@@ -7,8 +7,7 @@ const log = new kv.logger("who-is-who");
 
 let port = process.env.PORT || "80";
 
-let endpoint = process.env.AWS_DYNAMO_ENDPOINT ||
-  "https://dynamodb.us-west-1.amazonaws.com/";
+let endpoint = process.env.AWS_DYNAMO_ENDPOINT || "https://dynamodb.us-west-1.amazonaws.com/";
 let region = process.env.AWS_DYNAMO_REGION || "us-west-1";
 let accessKeyId = process.env.AWS_ACCESS_KEY_ID;
 let secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
@@ -21,16 +20,16 @@ const storage = require("./storage/dynamodb")(
   accessKeyId,
   secretAccessKey,
   tableNameSuffix,
-  dynamoReadWriteCapacity
+  dynamoReadWriteCapacity,
 );
 const db = require("./db")(storage);
 const router = require("./router")(db);
 
 let app = express();
 
-app.use(kv.middleware({source: "who-is-who"}));
+app.use(kv.middleware({ source: "who-is-who" }));
 app.use(bodyParser.json());
 
 app.use(router).listen(port);
 
-log.infoD("server ready", {"listening on": port});
+log.infoD("server ready", { "listening on": port });
