c31a31a8762e5170bbdde4db287d82f411020944
Mark Cabanero <mark.cabanero@clever.com>
2023-05-23 15:27:41 -0700

    chore: Update the references to the new OpenTelemetry packages

--- tracing.ts
+++ tracing.ts
@@ -1,20 +1,20 @@
 // adapted from:
 // https://github.com/open-telemetry/opentelemetry-js/blob/main/getting-started/ts-example/README.md
 // https://github.com/open-telemetry/opentelemetry-js/tree/main/packages/opentelemetry-exporter-collector-grpc
-import { NodeTracerProvider } from "@opentelemetry/node";
+import { NodeTracerProvider } from "@opentelemetry/sdk-trace-node";
 import { registerInstrumentations } from "@opentelemetry/instrumentation";
 import { ExpressInstrumentation } from "@opentelemetry/instrumentation-express";
 import { HttpInstrumentation } from "@opentelemetry/instrumentation-http";
-import { SimpleSpanProcessor } from "@opentelemetry/tracing";
-import { CollectorTraceExporter } from "@opentelemetry/exporter-collector-grpc";
+import { SimpleSpanProcessor } from "@opentelemetry/sdk-trace-base";
+import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-grpc";
 
 const provider: NodeTracerProvider = new NodeTracerProvider({});
-const exporter = new CollectorTraceExporter();
+const exporter = new OTLPTraceExporter();
 provider.addSpanProcessor(new SimpleSpanProcessor(exporter));
-
-provider.register();
 
 registerInstrumentations({
   tracerProvider: provider,
   instrumentations: [new ExpressInstrumentation(), new HttpInstrumentation()],
 });
+
+provider.register();
