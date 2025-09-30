0d892d296544dde066fa998352a751f16e15f9c5
Mark Cabanero <4522043+mcab@users.noreply.github.com>
2025-09-30 12:09:14 -0700

    SECNG-4265 | Remove capacity check (#144)
    * bug: Remove capacity check
    
    See c03cad399e999500aac997791919f88505fccd8c, where underlying bug
    comes from failing to properly validate provisioned read/write capacity.
    Since we've changed to on-demand, this value of read/write capacity
    is now 0, which is dangerous to try and coerce into actually zero
    due to JavaScript convention.
    
    * chore: Remove unused environment variable
    
    * chore: Patch table provisioned output during testing

--- server.js
+++ server.js
@@ -9,9 +9,8 @@ let port = process.env.PORT || "80";
 
 let endpoint = process.env.AWS_DYNAMO_ENDPOINT || "https://dynamodb.us-west-1.amazonaws.com/";
 let region = process.env.AWS_DYNAMO_REGION || "us-west-1";
-let dynamoReadWriteCapacity = parseInt(process.env.DYNAMO_READ_WRITE_CAPACITY, 10) || 0;
 
-const storage = require("./storage/dynamodb")(endpoint, region, dynamoReadWriteCapacity);
+const storage = require("./storage/dynamodb")(endpoint, region);
 const db = require("./db")(storage);
 const router = require("./router")(db);
 
